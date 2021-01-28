18ba2c5c9ea1f59a143b0246346b219a64b04e1e
828200 <828200@SAIT224098IT.ACDM.DS.SAIT.CA>
2021-01-27 23:17:28 -0700

    new updated version of an age calculator.

--- src/java/servlet/AgeCalculatorServlet.java
+++ src/java/servlet/AgeCalculatorServlet.java
@@ -21,22 +21,15 @@ public class AgeCalculatorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-      getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request,response);
+        getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp")
+              .forward(request,response);
     }
 
+  
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String age=request.getParameter("age");
-        request.setAttribute("age",age);
-      if(age==null||age.equals("")||age.contains("[a-zA-Z]+") == true )
-      {
-           request.setAttribute("message","please, only use numbers and do not leave it blank!");
-          getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request,response);
-           return;
-      }
-       getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request,response);
+        
     }
 
-    
 }
